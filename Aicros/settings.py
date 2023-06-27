310f00556a457f0d7d9b59ca60fdbe5dfedabf69
abeldarrain <abeldarrain@aicros.cu>
2023-06-27 08:35:15 -0400

    asd

--- Aicros/settings.py
+++ Aicros/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR/'static' ]
+STATICFILES_DIRS = ['aicrosv2.onrender.com/static/' ]
 
 STATIC_ROOT = 'static_Server'
 
